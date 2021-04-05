869351a1bdf3e6b824c6804139873516f47f5933
Emrick Sinitambirivoutin <emrick.sinitambirivoutin@grenoble-inp.org>
2021-04-05 13:03:07 +0200

    make colors configurable

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {config} from "./data";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
+
+// Setup colors according to config in data
+document.documentElement.style.setProperty('--primary',config.primaryColor);
+document.documentElement.style.setProperty('--secondary',config.secondaryColor);
+document.documentElement.style.setProperty('--light',config.lightColor);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
