b7bb9e9ff5d3d568c0a6123e3e22575fd3813b16
Lionel Nébot Janvier <35747326+lionel-nj@users.noreply.github.com>
2021-05-23 16:46:18 -0400

    remove support for landing page (#37)

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import Menu from "./components/menu/Menu";
-import Landing from "./components/landing/Landing";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Switch>
-        <Route path="/" exact component={() => <Landing/>}/>
         <Route path="/the-moonrise-dinner"
           exact component={() => <Menu data_path="the-moonrise-dinner"/>}
         />
