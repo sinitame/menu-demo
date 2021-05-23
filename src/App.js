0eff2e0e5d8dcdfe26c5b8e7ea6244a544c02f26
Emrick Sinitambirivoutin <emrick.sinitambirivoutin@grenoble-inp.org>
2021-05-23 13:19:50 +0200

    App refactoring

--- src/App.js
+++ src/App.js
@@ -2,27 +2,40 @@ import React, {Component} from "react";
 import {Provider} from "./Context.jsx";
 import MediaCard from "./components/Card.jsx";
 import {Modal} from "./components/Modal.jsx";
-import "./styles.css";
-import Content from "./Content.jsx";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import Home from "./Home.jsx";
+import Logo from "./components/Logo.jsx";
+import Mains from "./components/MainsSection.jsx";
+import ExtraSection from "./components/ExtraSection.jsx";
 
-function Menu({clickHandler}) {
+import "./styles.css";
+
+function Menu({clickHandler, data_path}) {
+  let data = require(`./data/${data_path}/data.json`)
+
+  // Set menu colors from config
+  let config = require(`./data/${data_path}/config.json`)
+  document.documentElement.style.setProperty('--primary',config.primaryColor)
+  document.documentElement.style.setProperty('--secondary',config.secondaryColor)
+  document.documentElement.style.setProperty('--light',config.lightColor)
   return (
       <Provider>
-        <Router>
-          <Switch>
-            <Route path="/" exact component={() => <Home/>}/>
-            <Route path="/the-moonrise-dinner"
-                   exact component={() =>
-                <Content data_path="the-moonrise-dinner"
-                         clickHandler={clickHandler}/>}/>
-            <Route path="/another-dinner"
-                   exact component={() =>
-                <Content data_path="another-dinner"
-                         clickHandler={clickHandler}/>}/>
-          </Switch>
-        </Router>
+        <div className="menu">
+          <Logo/>
+          <div className="right">
+            <Mains type="Starters" meals={data.starters}
+                  clickHandler={clickHandler}/>
+            <Mains type="Main" meals={data.mains} clickHandler={clickHandler}/>
+            <Mains type="Desserts" meals={data.deserts}
+                  clickHandler={clickHandler}/>
+          </div>
+          <div className="left">
+            <aside className="aside">
+              <ExtraSection type="Sides" extras={data.sides}
+                            clickHandler={clickHandler}/>
+              <ExtraSection type="Drinks" extras={data.drinks}
+                            clickHandler={clickHandler}/>
+            </aside>
+          </div>
+        </div>
       </Provider>
   );
 }
@@ -53,10 +66,9 @@ class App extends Component {
         <div>
           <Modal show={this.state.show} bg_img_path={this.state.img}>
             <MediaCard handleClose={this.hideModal} img_path={this.state.img}
-                       name={this.state.name}
-                       description={this.state.description}/>
+                       name={this.state.name} description={this.state.description}/>
           </Modal>
-          <Menu clickHandler={this.showModal}/>
+          <Menu clickHandler={this.showModal} data_path={this.props.data_path}/>
         </div>
 
     );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import App from "./App";
+import Home from "./Home";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/" exact component={() => <Home/>}/>
+        <Route path="/the-moonrise-dinner"
+          exact component={() => <App data_path="the-moonrise-dinner"/>}
+        />
+        <Route path="/another-dinner"
+          exact component={() => <App data_path="another-dinner"/>}
+      />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   rootElement
 );
